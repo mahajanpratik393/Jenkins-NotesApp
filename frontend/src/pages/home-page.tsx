663d50e7e6372c7dc980386313607e0d092c120b
Bijendar <56512231+Bijendar@users.noreply.github.com>
2025-04-19 17:22:36 +0530

    Update home-page.tsx

--- frontend/src/pages/home-page.tsx
+++ frontend/src/pages/home-page.tsx
@@ -5,20 +5,25 @@ import PostCard from '@/components/post-card';
 import Post from '@/types/post-type';
 import { PostCardSkeleton } from '@/components/skeletons/post-card-skeleton';
 import Header from '@/layouts/header-layout';
+
 function HomePage() {
   const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
-   console.log("VITE_API_PATH:", import.meta.env.VITE_API_PATH);  // Log the value of VITE_API_PATH
+    console.log("VITE_API_PATH:", import.meta.env.VITE_API_PATH);  // Log the value of the env variable
     axios
       .get(import.meta.env.VITE_API_PATH + '/api/posts')
       .then((response) => {
-        setPosts(response.data);
+        console.log("Fetched posts:", response.data);  // Log the fetched data
+        setPosts(response.data);  // Set the posts data
       })
       .catch((error) => {
-        console.error(error);
+        console.error("Error fetching posts:", error);  // Log any errors
       });
   }, []);
+
+  // Log the posts state after it's updated
+  console.log("Posts Data:", posts);  // Verify if posts data is updated
 
   return (
     <div className="w-full cursor-default bg-light dark:bg-dark">
