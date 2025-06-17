39ae19d50b8601369892953923eb106936e42341
farhan24680 <147843222+farhan24680@users.noreply.github.com>
2025-06-17 18:56:57 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,34 +1,28 @@
+# SSH key pair
 resource "aws_key_pair" "deployer" {
-  key_name   = "terra-automate-key"
-  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
+  key_name   = "key"
+  public_key = file("~/.ssh/key.pub")
 }
 
-resource "aws_default_vpc" "default" {
+# Default VPC (assumes AWS default VPC exists)
+resource "aws_default_vpc" "default" {}
 
-}
-
+# Security group allowing limited access
 resource "aws_security_group" "allow_user_to_connect" {
   name        = "allow TLS"
   description = "Allow user to connect"
   vpc_id      = aws_default_vpc.default.id
+
   ingress {
-    description = "port 22 allow"
+    description = "Allow SSH from my IP"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = " allow all outgoing traffic "
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["103.45.67.89/32"]  # Replace with your public IP
   }
 
   ingress {
-    description = "port 80 allow"
+    description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -36,10 +30,18 @@ resource "aws_security_group" "allow_user_to_connect" {
   }
 
   ingress {
-    description = "port 443 allow"
+    description = "Allow HTTPS"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -47,17 +49,19 @@ resource "aws_security_group" "allow_user_to_connect" {
     Name = "mysecurity"
   }
 }
-
+# EC2 instance using the default VPC and security group
 resource "aws_instance" "testinstance" {
   ami             = var.ami_id
   instance_type   = var.instance_type
   key_name        = aws_key_pair.deployer.key_name
   security_groups = [aws_security_group.allow_user_to_connect.name]
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
   tags = {
     Name = "Automate"
-  }
-  root_block_device {
-    volume_size = 30 
-    volume_type = "gp3"
   }
 }
